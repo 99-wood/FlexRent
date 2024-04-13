6efb257553ca07dce4581a4d04f205550bcf5de3
Featherwit419 <mhzg03293@.com>
2024-04-13 14:10:34 +0800

    hash

--- head/city.h
+++ head/city.h
@@ -3,7 +3,8 @@
 
 #include "typedefine.h"
 #include "area.h"
-
+#include "intlist.h"
+#include <string.h>
 struct City{
     char * name;
     struct AreaList AreaList;
@@ -18,4 +19,12 @@ struct CityList{
     struct CityListNode head;
     int cnt;
 };
+//CityList创建、初始化
+CityList* newCityList();
+//新CityNode创建
+City* newCityNode();
+//查询City下的Area,即用即删
+AreaList* findAreaInCity(City* city,const char* c);
+
+
 #endif
---
+++ src/hash.c
@@ -0,0 +1,19 @@
+#include "hash.h"
+
+struct HashValue makeHashValue(char* c){
+	struct HashValue h;
+	long long value1=0,value2=0,value;
+	int len=strlen(c);
+	for(int i=0;i<len;i++){
+		value1 = (value1*256 + (int)c[i])%M1;
+	}
+	for(int i=0;i<len;i++){
+		value2 = (value2*256 + (int)c[i])%M2;
+	}
+	h.hash1 =value1,h.hash2 =value2;
+	return h;
+}
+
+int cmpHashValue(struct HashValue h1,struct HashValue h2){
+
+}
